58bcbff7df8c79d5663f63ea7173149dfc6531b9
Kushagra Rajpoot <kushagra305@gmail.com>
2025-06-29 08:27:32 +0000

    Can we reatrict the canvs till the end to left nav,

--- src/components/ui/sidebar.tsx
+++ src/components/ui/sidebar.tsx
@@ -230,7 +230,7 @@ const Sidebar = React.forwardRef<
         {/* This is what handles the sidebar gap on desktop */}
         <div
           className={cn(
-            "duration-200 relative h-full bg-transparent transition-[width] ease-linear",
+            "duration-200 relative h-full w-[--sidebar-width] bg-transparent transition-[width] ease-linear",
             "group-data-[collapsible=offcanvas]:w-0",
             "group-data-[side=right]:rotate-180",
             variant === "floating" || variant === "inset"
